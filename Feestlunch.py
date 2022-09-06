3b4e941012b914a5c8390a7aec2ad4b03daa7196
Deviyano <99074836@mydavinci.nl>
2022-09-06 20:32:59 +0200

    from importlib import import_module import math
    acroissantjes = 17
    croissantjes = 0,39*acroissantjes
    
    astokbrood = 2
    stokbrood = 2,78*astokbrood
    
    akorting = 3
    korting = 0,50*akorting
    print(stokbrood + croissantjes  korting)

---
+++ Feestlunch.py
@@ -0,0 +1,12 @@
+from importlib import import_module
+import math
+
+acroissantjes = 17
+croissantjes = 0,39*acroissantjes
+
+astokbrood = 2
+stokbrood = 2,78*astokbrood
+
+akorting = 3
+korting = 0,50*akorting
+print(stokbrood + croissantjes  korting)
