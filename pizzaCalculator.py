c9a0ab737dfa877ef9c51e80f963bb67c877f495
Deviyano <99074836@mydavinci.nl>
2022-09-12 10:30:14 +0200

    pizzaCalculator

---
+++ pizzaCalculator.py
@@ -0,0 +1,23 @@
+from ast import If
+from logging.handlers import TimedRotatingFileHandler
+import math
+
+small=6.99
+medium=9.99
+large=12.99
+
+keuze = input("Wilt u een small of medium of large pizza :")
+aantal = input("hoeveel pizza's wilt u :")
+
+if keuze == "small":
+    prijs = int (aantal)*6.99
+
+if keuze == "medium":
+    prijs = int (aantal)*9.99
+    
+if keuze =="large":
+    prijs= int (aantal)*12.99
+
+print(" ----------------------------")   
+print("|uw totale bedrag is",prijs," |")
+print(" ----------------------------") 
