439a95fad114ceced6180e0f7ea984d3db2c4bb4
Deviyano <99074836@mydavinci.nl>
2022-09-12 14:22:46 +0200

    Min en Max

--- else-statement.py
+++ else-statement.py
@@ -3,13 +3,17 @@ b=input("getal: ")
 
 if a>b :
  max=a 
+ min=b
  print("a is het grootste getal:",max,"")
 
 elif a<b :
     min=a 
+    max=b
     print("a is het kleinste getal:",min,"")
 
 else :
  a==b 
 print("a en b zijn even groot")
- 
+
+print("Het minimum is: ",min,"")
+print("Het maximum is: ",max,"")
