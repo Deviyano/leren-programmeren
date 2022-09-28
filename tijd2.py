1f396a1afc96820e74be3c71c38690c93690f32e
Deviyano <99074836@mydavinci.nl>
2022-09-28 12:30:27 +0200

    dagen tot dan.py

---
+++ tijd2.py
@@ -0,0 +1,22 @@
+import math
+
+am=True
+pm=True
+tijd=0
+tijd = int(tijd)
+while am == True:
+    print("",tijd,"am")
+    tijd=tijd+1
+    if tijd <12 :
+        am=True
+    else:
+        am=False
+while pm == True:
+    print("",tijd,"pm")
+    tijd=tijd+1
+    if tijd <24 :
+        pm=True
+    else:
+        pm=False
+
+
