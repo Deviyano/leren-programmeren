8b7cdd636b0e0979d092a9a6a796d8902426c850
Deviyano <99074836@mydavinci.nl>
2022-10-05 11:14:55 +0200

    coin.py

---
+++ feeest.py
@@ -0,0 +1,11 @@
+gastheer = input("Wat is de naam van de gastheer: ")
+gasten = 4
+gasten = int(gasten)
+drank = True
+chips = False
+
+if gastheer == True and drank == True and gasten == 0 or gastheer == True and drank == True and gasten <=12 and gasten >=5 or gasten >=5 and gasten <=12 and drank and chips:
+    print ("start de party")
+else:
+    print ("no party")
+    
