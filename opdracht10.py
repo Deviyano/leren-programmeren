3872b347b0b0107a1e5b904f844d215875fe8908
Deviyano <112937552+Deviyano@users.noreply.github.com>
2023-05-04 10:07:17 +0200

    Delete opdracht10.py

--- opdracht10.py
+++
@@ -1,5 +0,0 @@
-from fruitmand import fruitmand
-lijst={}
-for x in range(len(fruitmand)):
-    lijst.update({fruitmand[x].get("weight")/1000: fruitmand[x].get("name")})
-print(sorted(lijst.items(),reverse=True))
